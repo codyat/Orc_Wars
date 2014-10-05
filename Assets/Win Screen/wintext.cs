d79905409ea9f36ec2c7e97eb8f5e16250fa7ae4
Cody Troyer <codyatroyer@yahoo.com>
2014-10-04 17:09:43 -0700

    third commit

---
+++ Assets/Win Screen/wintext.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using RTS;
+
+public class wintext : MonoBehaviour {
+	public TextMesh text;
+
+	// Use this for initialization
+	void Start () {
+		if(ResourceManager.previousLevel == 11)
+			text.text = "Victory!";
+	}
+}
